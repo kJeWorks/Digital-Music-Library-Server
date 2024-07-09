b8ab42de29932d569bf1d326a4d6371cf321b1e4
norbikj2002 <kesetjoaca@gmail.com>
2024-07-09 17:20:21 +0300

    created the entities for songs, albums and bands

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BandsModule } from './bands/bands.module';
+import { AlbumsModule } from './albums/albums.module';
+import { SongsModule } from './songs/songs.module';
 
 @Module({
-  imports: [],
+  imports: [BandsModule, AlbumsModule, SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
